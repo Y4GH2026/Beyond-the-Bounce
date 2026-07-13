986b2e9899f07ea21413ecea64637310ff0f7521
Y4GH2026 <youthforglobalhealth@gmail.com>
2026-07-13 16:18:19 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME='btb-v3';
+const ASSETS=['./','./index.html','./banner.png','./manifest.json'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
